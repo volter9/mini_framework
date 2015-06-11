78e9c830a2d68560e884fcdf60e33f8f09e26d51
volter9 <volter925@gmail.com>
2015-06-10 18:17:07 -0700

    Remove unnecassary files

--- views/default/html/blocks/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>&copy; Твой Копирайт 2015 &mdash; Поуред бай <a href="https://github.com/Volter9/mini_framework">mini_framework</a></p>
-</footer>
-<!-- templates/default/html/blocks/footer.php -->
--- views/default/html/blocks/head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="UTF-8"/>
-<title><?php echo $title ?> - Сайт</title>
-
-<link href="<?php echo asset_url('css/main.css') ?>" rel="stylesheet" type="text/css"/>
-<!-- templates/default/html/blocks/head.php -->
